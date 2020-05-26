31e214cbbfafb0be80b284d96a1da4f7f2370033
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-05-26 19:43:08 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
  # resources :secrets, only: [:show]
+ 
+  get '/secrets/:id', to: 'secrets#show'
 end
