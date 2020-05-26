8fa9596e32d94fffa920cc15a7c15165676c9f85
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-05-26 20:08:17 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
  end
  
  def create 
-    binding.pry
+    #binding.pry
     name = params[:name]
     if !name || name.empty?
       redirect_to controller: 'sessions', action: 'new'
